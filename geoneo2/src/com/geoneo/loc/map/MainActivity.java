74387a7dbd91e50c4c726f6318ab09269502d4da
Piotr Gryziak <gryziak@gmail.com>
2013-10-29 16:23:46 +0100

    Asd

---
+++ geoneo2/src/com/geoneo/loc/map/MainActivity.java
@@ -0,0 +1,34 @@
+package com.geoneo.loc.map;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends Activity {
+	static final LatLng GDANSK = new LatLng(54.359030, 18.6443);
+	private GoogleMap map;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		// Move the camera instantly to hamburg with a zoom of 15.
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(GDANSK, 15));
+
+		// Zoom in, animating the camera.
+		map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+  }
+
+} 
